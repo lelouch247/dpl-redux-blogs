7b001c9eba326cd4898c4fc9752657668fe678da
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-06-27 15:00:59 -0600

    can add things to list

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const BlogList = () => {
-  return (
-    <div>
-    </div>
-  )
+const BlogList = ({ blogs }) => (
+  <ul>
+    { blogs.map ( (t,i) => {
+      return (
+        <li key={i}>
+          {t}
+        </li>
+      )
+    })
+  }
+  </ul>
+)
+
+const mapStateToProps = (state) => {
+  return { blogs: state.blogs }
 }
 
 
-export default BlogList;
+
+export default connect(mapStateToProps)(BlogList);
